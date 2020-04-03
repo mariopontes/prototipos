4e8a530133dd0aba996ca0522772e7cf512188e8
mario pontes <mario.canys@hotmail.com>
2020-04-02 22:50:51 -0300

    --

--- src/app/pages/dual-list/dual-list.component.ts
+++ src/app/pages/dual-list/dual-list.component.ts
@@ -14,6 +14,9 @@ export class DualListComponent implements OnInit {
   agrupamento = []
   agrupamentoAdd = []
   listInstitutos: any;
+  contextos = []
+  cards = []
+  politicas = []
 
 
   constructor(
@@ -37,58 +40,48 @@ export class DualListComponent implements OnInit {
   getAgrupamentos() {
     this.http.get('http://localhost:9000/dual-list')
       .subscribe((data: any) => {
-        debugger
         this.agrupamento = data
+        this.contextos = data;
+        this.getDataFilter();
       })
   }
 
   addItem(contexto: any, card?: any, politica?: any) {
 
     if (contexto && !card && !politica) {
-      contexto.selected = true;
-      this.agrupamentoAdd.push(contexto)
+
     }
 
-
     if (contexto && card && !politica) {
-      contexto.selected = true;
-      card.selected = true;
-
-      this.agrupamentoAdd.push(contexto)
-
-      this.agrupamentoAdd.forEach(e => {
-        e.cards.forEach((x, i) => {
-          if (!x.selected) {
-            e.cards.splice(i, 1)
-          }
-        })
-      })
 
     }
 
     if (contexto && card && politica) {
+      //
     }
 
   }
 
-  getDataFilter(value: Contexto[]) {
-    this.contextoFilter = value;
-    this.contextoFilter.map(e => {
+  getDataFilter() {
+    this.contextos.map(e => {
 
       e.cards.map(c => {
-        this.cardFilter.push(c)
+        this.cards.push(c)
 
         c.politicas.map(p => {
           let addOn = true;
 
-          this.politicasFilter.forEach(o => o.id == p.id ? addOn = false : null);
-          addOn ? this.politicasFilter.push(p) : null;
+          this.politicas.forEach(o => o.id == p.id ? addOn = false : null);
+          addOn ? this.politicas.push(p) : null;
         })
 
       })
     })
 
-    this.createForm();
+    console.log(this.contextos)
+    console.log(this.cards)
+    console.log(this.politicas)
+
   }
 
 }
